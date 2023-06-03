1ff7ad5d8fdb9efb256ee4e6823ed305ebef17c6
Natalie Pinnock <natpinnock@gmail.com>
2023-06-03 07:58:55 +0100

    post request added for groups

--- src/app/api/groups/route.ts
+++ src/app/api/groups/route.ts
@@ -1,8 +1,28 @@
 import { NextResponse, NextRequest } from "next/server";
 import prisma from "@/utils/prisma";
+import { auth } from "@clerk/nextjs";
+import { v4 as uuidv4 } from "uuid";
 
 export async function GET(request: NextRequest) {
   const groups = await prisma.group.findMany();
-  console.log(groups);
   return new Response(JSON.stringify(groups));
+}
+
+export async function POST(request: NextRequest) {
+  const { userId } = auth();
+  if (!userId) {
+    return new Response("Unauthorized", { status: 401 });
+  }
+  const { groupName, numberOfStudents, notes } = await request.json();
+  const group = await prisma.group.create({
+    data: {
+      id: uuidv4(),
+      userId,
+      groupName,
+      numberOfStudents,
+      notes,
+    },
+  });
+
+  return new Response(JSON.stringify(group));
 }
--- src/app/groups/page.tsx
+++ src/app/groups/page.tsx
@@ -13,13 +13,41 @@ export default function Groups() {
       .then((data) => setGroups(data));
   }, []);
   return (
-    <div>
-      <h1>Groups</h1>
-      <ul>
-        {groups.map((group) => (
-          <li key={group.id}>{group.groupName}</li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <div>
+        <h1>Groups</h1>
+        <ul>
+          {groups.map((group) => (
+            <li key={group.id}>{group.groupName}</li>
+          ))}
+        </ul>
+      </div>
+
+      <div>Add a group</div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          //perform a post request to /api/groups
+          fetch("/api/groups", {
+            method: "POST",
+            body: JSON.stringify({
+              groupName: e.currentTarget.groupName.value,
+              numberOfStudents: e.currentTarget.numberOfStudents.value,
+              notes: e.currentTarget.notes.value,
+            }),
+          })
+            .then((response) => response.json())
+            .then((data) => {
+              console.log(data);
+              setGroups([...groups, data]);
+            });
+        }}
+      >
+        <input type="text" name="groupName" />
+        <input type="text" name="numberOfStudents" />
+        <input type="text" name="notes" />
+        <button type="submit">Add</button>
+      </form>
+    </>
   );
 }
